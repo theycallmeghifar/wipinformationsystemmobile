b945d17745fd8c75a2c59d03f309b5d60db20a39
Al Ghifari <127167868+theycallmeghifar@users.noreply.github.com>
2025-03-10 22:36:46 +0700

    first commit

---
+++ app/src/main/java/id/co/fim/wipinformationsystemmobile/MainActivity.java
@@ -0,0 +1,14 @@
+package id.co.fim.wipinformationsystemmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
